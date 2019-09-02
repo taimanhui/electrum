7c16baeeec235c673e52af865ad27c422ce6d851
liyan <liyan@liyandeMacBook-Pro.local>
2019-09-02 17:10:45 +0800

    Ref:T6239 Port applet source
    Summary: Ref:T6239
    
    Test Plan: add test interface
    
    Reviewers: daniel, lishuai
    
    Reviewed By: daniel
    
    Differential Revision: https://phabricator.haobtc.com/D5526

--- BitcoinWallet/src/com/ledger/wallet/Crypto.java
+++ BitcoinWallet/src/com/ledger/wallet/Crypto.java
@@ -22,20 +22,11 @@ package com.ledger.wallet;
 import javacard.framework.JCSystem;
 import javacard.framework.Util;
 import javacard.security.CryptoException;
-import javacard.security.DESKey;
-import javacard.security.AESKey;
-import javacard.security.ECKey;
 import javacard.security.ECPrivateKey;
-import javacard.security.ECPublicKey;
-import javacard.security.HMACKey;
-import javacard.security.KeyAgreement;
 import javacard.security.KeyBuilder;
-import javacard.security.KeyPair;
-import javacard.security.MessageDigest;
 import javacard.security.RandomData;
 import javacard.security.Signature;
-import javacardx.crypto.Cipher;
-
+import javacardx.framework.math.BigNumber;
 /**
  * Hardware Wallet crypto tools
  * @author BTChip
@@ -43,12 +34,15 @@ import javacardx.crypto.Cipher;
  */
 public class Crypto {
 	
-	// Java Card constants might be off for some platforms - recheck with your implementation
-	//private static final short HMAC_SHA512_SIZE = KeyBuilder.LENGTH_HMAC_SHA_512_BLOCK_128; 
-	private static final short HMAC_SHA512_SIZE = (short)(32 * 8);
-    
+	private static final byte[] MAX_AVAILABLE_SIGNATURE_S = {
+            (byte)0x7F,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,
+            (byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,
+            (byte)0x5D,(byte)0x57,(byte)0x6E,(byte)0x73,(byte)0x57,(byte)0xA4,(byte)0x50,(byte)0x1D,
+            (byte)0xDF,(byte)0xE9,(byte)0x2F,(byte)0x46,(byte)0x68,(byte)0X1B,(byte)0x20,(byte)0xA0
+    };
+
     public static void init() {
-        scratch = JCSystem.makeTransientByteArray((short)1, JCSystem.CLEAR_ON_DESELECT);
+        sInSignature = JCSystem.makeTransientByteArray((short)256, JCSystem.CLEAR_ON_DESELECT);
         random = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM);
         try {
             // ok, let's save RAM
@@ -67,75 +61,7 @@ public class Crypto {
                 Secp256k1.setCommonCurveParameters(transientPrivate);
             }
         }
-        digestFull = MessageDigest.getInstance(MessageDigest.ALG_SHA_256, false);
-        digestAuthorization = MessageDigest.getInstance(MessageDigest.ALG_SHA_256, false);
-        digestScratch = MessageDigest.getInstance(MessageDigest.ALG_SHA_256, false);
-        blobEncryptDecrypt = Cipher.getInstance(Cipher.ALG_DES_CBC_NOPAD, false);
         signature = Signature.getInstance(Signature.ALG_ECDSA_SHA_256, false);
-        try {
-            digestRipemd = MessageDigest.getInstance(MessageDigest.ALG_RIPEMD160, false);
-        }
-        catch(CryptoException e) {
-            // A typical Java Card implementation will not support RIPEMD160 - we deal with it
-        }
-        try {
-        	digestSha512 = MessageDigest.getInstance(MessageDigest.ALG_SHA_512, false); 
-        }
-        catch(CryptoException e) {
-        	sha512 = new SHA512();
-        }
-        try {
-        	signatureHmac = Signature.getInstance(Signature.ALG_HMAC_SHA_512, false);
-        	try {
-                // ok, let's save RAM        		
-        		keyHmac = (HMACKey)KeyBuilder.buildKey(KeyBuilder.TYPE_HMAC_TRANSIENT_DESELECT, HMAC_SHA512_SIZE, false);
-                keyHmac2 = (HMACKey)KeyBuilder.buildKey(KeyBuilder.TYPE_HMAC_TRANSIENT_DESELECT, HMAC_SHA512_SIZE, false);
-        	}
-        	catch(CryptoException e) {
-        		try {
-                    // ok, let's save a bit less RAM        			
-        			keyHmac = (HMACKey)KeyBuilder.buildKey(KeyBuilder.TYPE_HMAC_TRANSIENT_RESET, HMAC_SHA512_SIZE, false);
-                    keyHmac2 = (HMACKey)KeyBuilder.buildKey(KeyBuilder.TYPE_HMAC_TRANSIENT_RESET, HMAC_SHA512_SIZE, false);
-        		}
-        		catch(CryptoException e1) {
-                    // ok, let's test the flash wear leveling \o/        			
-        			keyHmac = (HMACKey)KeyBuilder.buildKey(KeyBuilder.TYPE_HMAC, HMAC_SHA512_SIZE, false);
-                    keyHmac2 = (HMACKey)KeyBuilder.buildKey(KeyBuilder.TYPE_HMAC, HMAC_SHA512_SIZE, false);
-        		}
-        	}        	
-        }
-        catch(CryptoException e) {
-        	signatureHmac = null;
-        }
-        // Optional initializations if no proprietary API is available
-        try {
-        	keyAgreement = KeyAgreement.getInstance(KeyAgreement.ALG_EC_SVDP_DH_PLAIN, false);
-        }
-        catch(CryptoException e) {
-        	// Not having the KeyAgreement API is manageable if there is a proprietary API to recover public keys
-            // and if the airgapped personalization can be skipped
-        	// Otherwise there should be a remote secure oracle performing public derivations and sending back results
-        }
-        try {
-            publicKey = (ECPublicKey)KeyBuilder.buildKey(KeyBuilder.TYPE_EC_FP_PUBLIC, KeyBuilder.LENGTH_EC_FP_256, false);
-            Secp256k1.setCommonCurveParameters(publicKey);        	
-        }
-        catch(CryptoException e) {
-        }                
-        try {
-                keyPair = new KeyPair(
-                        (ECPublicKey)KeyBuilder.buildKey(KeyBuilder.TYPE_EC_FP_PUBLIC, KeyBuilder.LENGTH_EC_FP_256, false),
-                        (ECPrivateKey)KeyBuilder.buildKey(KeyBuilder.TYPE_EC_FP_PRIVATE, KeyBuilder.LENGTH_EC_FP_256, false));
-                Secp256k1.setCommonCurveParameters((ECKey)keyPair.getPrivate());
-                Secp256k1.setCommonCurveParameters((ECKey)keyPair.getPublic());
-        }
-        catch(CryptoException e) {            
-        }
-        try {
-            blobEncryptDecryptAES = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
-        }
-        catch(CryptoException e) {            
-        }
     }
     
     public static void initTransientPrivate(byte[] keyBuffer, short keyOffset) {
@@ -144,75 +70,38 @@ public class Crypto {
         }
         transientPrivate.setS(keyBuffer, keyOffset, (short)32);    	
     }
-    
-    public static void signTransientPrivate(byte[] keyBuffer, short keyOffset, byte[] dataBuffer, short dataOffset, byte[] targetBuffer, short targetOffset) {
-    	initTransientPrivate(keyBuffer, keyOffset);
+    public static void preComputedHashSign(byte[] keyBuffer, short keyOffset, byte[] dataBuffer, short dataOffset, byte[] targetBuffer, short targetOffset) {
+        initTransientPrivate(keyBuffer, keyOffset);
         Util.arrayFillNonAtomic(keyBuffer, keyOffset, (short)32, (byte)0x00);
         // recheck with the target platform, initializing once instead might be possible and save a few flash write
         // (this part is unspecified in the Java Card API)
         signature.init(transientPrivate, Signature.MODE_SIGN);
-        signature.sign(dataBuffer, dataOffset, (short)32, targetBuffer, targetOffset);
+        signature.signPreComputedHash(dataBuffer, dataOffset, (short)32, targetBuffer, targetOffset);
+        checkSignatureS(targetBuffer);
         if (transientPrivateTransient) {
             transientPrivate.clearKey();
         }
     }
     
-    // following method is only used if no proprietary API is available
-    public static boolean verifyPublic(byte[] keyBuffer, short keyOffset, byte[] dataBuffer, short dataOffset, byte[] signatureBuffer, short signatureOffset) {
-    	publicKey.setW(keyBuffer, keyOffset, (short)65);
-    	signature.init(publicKey, Signature.MODE_VERIFY);
-    	try {
-    		return signature.verify(dataBuffer, dataOffset, (short)32, signatureBuffer, signatureOffset, (short)(signatureBuffer[(short)(signatureOffset + 1)] + 2));
-    	}
-    	catch(Exception e) {
-    		return false;
-    	}
-    }
-    
-    public static void initCipher(DESKey key, boolean encrypt) {
-        blobEncryptDecrypt.init(key, (encrypt ? Cipher.MODE_ENCRYPT : Cipher.MODE_DECRYPT), IV_ZERO, (short)0, (short)IV_ZERO.length);
-    }
-
-    public static void initCipherAES(AESKey key, boolean encrypt) {
-        blobEncryptDecryptAES.init(key, (encrypt ? Cipher.MODE_ENCRYPT : Cipher.MODE_DECRYPT), IV_ZERO_AES, (short)0, (short)IV_ZERO_AES.length);
-    }
-
-    public static byte getRandomByteModulo(short modulo) {
-        short rng_max = (short)(256 % modulo);
-        short rng_limit = (short)(256 - rng_max);
-        short candidate = (short)0;
-        do {
-            random.generateData(scratch, (short)0, (short)1);
-            candidate = (short)(scratch[0] & 0xff);
+    private static void checkSignatureS(byte[] targetBuffer) {
+        short sLenOffset = (short)(targetBuffer[3]&0x00ff + 4);
+        short sLen = (short)(targetBuffer[sLenOffset] & 0x00ff);
+        sLenOffset++;
+        Util.arrayCopy(targetBuffer, sLenOffset, sInSignature, (short)0, sLen);
+        // when S in signature > 0x7FFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF 5D576E73 57A4501D DFE92F46 681B20A0
+        // use S'(0xFFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE BAAEDCE6 AF48A03B BFD25E8C D0364141 - S)  replace S
+        if (Util.arrayCompare(sInSignature, (short)0, MAX_AVAILABLE_SIGNATURE_S, (short)0, sLen) > 0) {
+            BigNumber maxS =  new BigNumber((short)32);
+            maxS.init(Secp256k1.SECP256K1_R, (short)0, (short)32, BigNumber.FORMAT_HEX);
+            maxS.subtract(sInSignature, (short) 0, sLen, BigNumber.FORMAT_HEX);
+            // S' = 0xFFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE BAAEDCE6 AF48A03B BFD25E8C D0364141 - S
+            maxS.toBytes(targetBuffer, sLenOffset, sLen, BigNumber.FORMAT_HEX);
         }
-        while(candidate > rng_limit);
-        return (byte)(candidate % modulo);
     }
-    
-    private static final byte[] IV_ZERO = { 0, 0, 0, 0, 0, 0, 0, 0 };
-    private static final byte[] IV_ZERO_AES = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-    private static byte[] scratch;
+    private static byte[] sInSignature;
     protected static ECPrivateKey transientPrivate;    
     protected static boolean transientPrivateTransient;    
     protected static Signature signature;
-    protected static Signature signatureHmac;
-    protected static HMACKey keyHmac;
-    protected static HMACKey keyHmac2; // duplicated because platforms don't like changing the key size on the fly
-    protected static MessageDigest digestFull;
-    protected static MessageDigest digestAuthorization;
-    protected static MessageDigest digestScratch;
-    protected static MessageDigest digestRipemd;
-    protected static MessageDigest digestSha512;
-    protected static SHA512 sha512;
     protected static RandomData random;
-    protected static Cipher blobEncryptDecrypt;    
-    protected static Cipher blobEncryptDecryptAES;
-
-    protected static KeyAgreement keyAgreement;
-    protected static KeyPair keyPair;
-    
-    // following variables are only used if no proprietary API is available
-    protected static ECPublicKey publicKey; 
-    
 }
