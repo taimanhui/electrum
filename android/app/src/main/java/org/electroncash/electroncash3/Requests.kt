42ac06bf64f5c8e49593163f7ac76f8ee9efc2f0
danielsocials <daniel.socials@gmail.com>
2019-11-07 09:58:40 +0800

    port native android support

---
+++ android/app/src/main/java/org/electroncash/electroncash3/Requests.kt
@@ -0,0 +1,200 @@
+package org.electroncash.electroncash3
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.observe
+import com.chaquo.python.PyObject
+import kotlinx.android.synthetic.main.amount_box.*
+import kotlinx.android.synthetic.main.request_detail.*
+import kotlinx.android.synthetic.main.requests.*
+
+
+class RequestsFragment : Fragment(R.layout.requests), MainFragment {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupVerticalList(rvRequests)
+        rvRequests.adapter = RequestsAdapter(activity!!)
+        TriggerLiveData().apply {
+            addSource(daemonUpdate)
+            addSource(settings.getString("base_unit"))
+        }.observe(viewLifecycleOwner, { refresh() })
+
+        btnAdd.setOnClickListener { newRequest(activity!!) }
+    }
+
+    fun refresh() {
+        val wallet = daemonModel.wallet
+        (rvRequests.adapter as RequestsAdapter).submitList(
+            if (wallet == null) null else RequestsList(wallet))
+    }
+}
+
+
+fun newRequest(activity: FragmentActivity) {
+    try {
+        val address = daemonModel.wallet!!.callAttr("get_unused_address")
+                      ?: throw ToastException(R.string.no_more)
+        showDialog(activity, RequestDialog(address.callAttr("to_storage_string").toString()))
+    } catch (e: ToastException) { e.show() }
+}
+
+
+class RequestsList(wallet: PyObject) : AbstractList<RequestModel>() {
+    val requests = wallet.callAttr("get_sorted_requests", daemonModel.config).asList()
+
+    override val size: Int
+        get() = requests.size
+
+    override fun get(index: Int) =
+        RequestModel(requests.get(index))
+}
+
+
+class RequestsAdapter(val activity: FragmentActivity)
+    : BoundAdapter<RequestModel>(R.layout.request_list) {
+
+    override fun onBindViewHolder(holder: BoundViewHolder<RequestModel>, position: Int) {
+        super.onBindViewHolder(holder, position)
+        holder.itemView.setOnClickListener {
+            showDialog(activity, RequestDialog(holder.item.address))
+        }
+    }
+}
+
+class RequestModel(val request: PyObject) {
+    val address = getField("address").toString()
+    val amount = formatSatoshis(getField("amount").toLong())
+    val timestamp = libUtil.callAttr("format_time", getField("time")).toString()
+    val description = getField("memo").toString()
+    val status = formatStatus(getField("status").toInt())
+
+    private fun formatStatus(status: Int): Any {
+        return app.resources.getStringArray(R.array.payment_status)[status]
+    }
+
+    private fun getField(key: String): PyObject {
+        return request.callAttr("get", key)!!
+    }
+}
+
+
+
+class RequestDialog() : AlertDialogFragment() {
+    val wallet by lazy { daemonModel.wallet!! }
+
+    init {
+        if (wallet.callAttr("is_watching_only").toBoolean()) {
+            throw ToastException(R.string.this_wallet_is)
+        }
+    }
+
+    val address by lazy {
+        clsAddress.callAttr("from_string", arguments!!.getString("address"))
+    }
+    val existingRequest by lazy {
+        wallet.callAttr("get_payment_request", address, daemonModel.config)
+    }
+
+    constructor(address: String): this() {
+        arguments = Bundle().apply { putString("address", address) }
+    }
+
+    override fun onBuildDialog(builder: AlertDialog.Builder) {
+        with (builder) {
+            setView(R.layout.request_detail)
+            setNegativeButton(android.R.string.cancel, null)
+            setPositiveButton(android.R.string.ok, null)
+            if (existingRequest != null) {
+                setNeutralButton(R.string.delete, null)
+            }
+        }
+    }
+
+    override fun onShowDialog() {
+        btnCopy.setOnClickListener {
+            copyToClipboard(getUri(), R.string.request_uri)
+        }
+        tvAddress.text = address.callAttr("to_ui_string").toString()
+        tvUnit.text = unitName
+
+        val tw = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) { updateUI() }
+        }
+        for (et in listOf(etAmount, etDescription)) {
+            et.addTextChangedListener(tw)
+        }
+        fiatUpdate.observe(this, { updateUI() })
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener { onOK() }
+
+        if (existingRequest != null) {
+            dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setOnClickListener {
+                showDialog(this, DeleteRequestDialog(address))
+            }
+        }
+    }
+
+    override fun onFirstShowDialog() {
+        val request = existingRequest
+        if (request != null) {
+            val model = RequestModel(request)
+            etAmount.setText(model.amount)
+            etDescription.setText(model.description)
+        }
+    }
+
+    private fun updateUI() {
+        showQR(imgQR, getUri())
+        amountBoxUpdate(dialog)
+    }
+
+    private fun getUri(): String {
+        var amount: Long? = null
+        try {
+            amount = amountBoxGet(dialog)
+        } catch (e: ToastException) {}
+        return libWeb.callAttr("create_URI", address, amount, description).toString()
+    }
+
+    private fun onOK() {
+        try {
+            val amount = amountBoxGet(dialog)
+            wallet.callAttr(
+                "add_payment_request",
+                wallet.callAttr("make_payment_request", address, amount, description),
+                daemonModel.config)
+            daemonUpdate.setValue(Unit)
+            dismiss()
+        } catch (e: ToastException) { e.show() }
+    }
+
+    val description
+        get() = etDescription.text.toString()
+}
+
+
+class DeleteRequestDialog() : AlertDialogFragment() {
+    constructor(addr: PyObject) : this() {
+        arguments = Bundle().apply {
+            putString("address", addr.callAttr("to_storage_string").toString())
+        }
+    }
+
+    override fun onBuildDialog(builder: AlertDialog.Builder) {
+        builder.setTitle(R.string.confirm_delete)
+            .setMessage(R.string.are_you_sure_you_wish_to_proceed)
+            .setPositiveButton(R.string.delete) { _, _ ->
+                daemonModel.wallet!!.callAttr("remove_payment_request",
+                                              makeAddress(arguments!!.getString("address")!!),
+                                              daemonModel.config)
+                daemonUpdate.setValue(Unit)
+                (targetFragment as RequestDialog).dismiss()
+            }
+            .setNegativeButton(android.R.string.cancel, null)
+    }
+}
---
+++ android/app/src/main/python/electroncash_gui/android/console.py
@@ -0,0 +1,255 @@
+from __future__ import absolute_import, division, print_function
+
+from code import InteractiveConsole
+import json
+import os
+from os.path import exists, join
+import pkgutil
+import unittest
+
+from electroncash import commands, daemon, keystore, simple_config, storage, tests, util
+from electroncash.i18n import _
+from electroncash.storage import WalletStorage
+from electroncash.wallet import (ImportedAddressWallet, ImportedPrivkeyWallet, Standard_Wallet,
+                                 Wallet)
+
+from android.preference import PreferenceManager
+
+
+CALLBACKS = ["banner", "blockchain_updated", "fee", "interfaces", "new_transaction",
+             "on_history", "on_quotes", "servers", "status", "verified2", "wallet_updated"]
+
+
+class AndroidConsole(InteractiveConsole):
+    """`interact` must be run on a background thread, because it blocks waiting for input.
+    """
+    def __init__(self, app, cmds):
+        namespace = dict(c=cmds, context=app)
+        namespace.update({name: CommandWrapper(cmds, name) for name in all_commands})
+        namespace.update(help=Help())
+        InteractiveConsole.__init__(self, locals=namespace)
+
+    def interact(self):
+        try:
+            InteractiveConsole.interact(
+                self, banner=(
+                    _("WARNING!") + "\n" +
+                    _("Do not enter code here that you don't understand. Executing the wrong "
+                      "code could lead to your coins being irreversibly lost.") + "\n" +
+                    "Type 'help' for available commands and variables."))
+        except SystemExit:
+            pass
+
+
+class CommandWrapper:
+    def __init__(self, cmds, name):
+        self.cmds = cmds
+        self.name = name
+
+    def __call__(self, *args, **kwargs):
+        return getattr(self.cmds, self.name)(*args, **kwargs)
+
+
+class Help:
+    def __repr__(self):
+        return self.help()
+
+    def __call__(self, *args):
+        print(self.help(*args))
+
+    def help(self, name_or_wrapper=None):
+        if name_or_wrapper is None:
+            return("Commands:\n" +
+                   "\n".join(f"  {cmd}" for name, cmd in sorted(all_commands.items())) +
+                   "\nType help(<command>) for more details.\n"
+                   "The following variables are also available: "
+                   "c.config, c.daemon, c.network, c.wallet, context")
+        else:
+            if isinstance(name_or_wrapper, CommandWrapper):
+                cmd = all_commands[name_or_wrapper.name]
+            else:
+                cmd = all_commands[name_or_wrapper]
+            return f"{cmd}\n{cmd.description}"
+
+
+# Adds additional commands which aren't available over JSON RPC.
+class AndroidCommands(commands.Commands):
+    def __init__(self, app):
+        super().__init__(AndroidConfig(app), wallet=None, network=None)
+        fd, server = daemon.get_fd_or_server(self.config)
+        if not fd:
+            raise Exception("Daemon already running")  # Same wording as in daemon.py.
+
+        # Initialize here rather than in start() so the DaemonModel has a chance to register
+        # its callback before the daemon threads start.
+        self.daemon = daemon.Daemon(self.config, fd, False)
+        self.network = self.daemon.network
+        self.network.register_callback(self._on_callback, CALLBACKS)
+        self.daemon_running = False
+
+    # BEGIN commands from the argparse interface.
+
+    def start(self):
+        """Start the daemon"""
+        self.daemon.start()
+        self.daemon_running = True
+
+    def status(self):
+        """Get daemon status"""
+        self._assert_daemon_running()
+        return self.daemon.run_daemon({"subcommand": "status"})
+
+    def stop(self):
+        """Stop the daemon"""
+        self._assert_daemon_running()
+        self.daemon.stop()
+        self.daemon.join()
+        self.daemon_running = False
+
+    def load_wallet(self, name, password=None):
+        """Load a wallet"""
+        self._assert_daemon_running()
+        path = self._wallet_path(name)
+        wallet = self.daemon.get_wallet(path)
+        if not wallet:
+            storage = WalletStorage(path)
+            if not storage.file_exists():
+                raise FileNotFoundError(path)
+            if storage.is_encrypted():
+                if not password:
+                    raise util.InvalidPassword()
+                storage.decrypt(password)
+
+            wallet = Wallet(storage)
+            wallet.start_threads(self.network)
+            self.daemon.add_wallet(wallet)
+
+    def close_wallet(self, name=None):
+        """Close a wallet"""
+        self._assert_daemon_running()
+        self.daemon.stop_wallet(self._wallet_path(name))
+
+    def create(self, name, password, seed=None, passphrase="", bip39_derivation=None,
+               master=None, addresses=None, privkeys=None):
+        """Create or restore a new wallet"""
+        path = self._wallet_path(name)
+        if exists(path):
+            raise FileExistsError(path)
+        storage = WalletStorage(path)
+
+        if addresses is not None:
+            wallet = ImportedAddressWallet.from_text(storage, addresses)
+        elif privkeys is not None:
+            wallet = ImportedPrivkeyWallet.from_text(storage, privkeys)
+        else:
+            if bip39_derivation is not None:
+                ks = keystore.from_bip39_seed(seed, passphrase, bip39_derivation)
+            elif master is not None:
+                ks = keystore.from_master_key(master)
+            else:
+                if seed is None:
+                    seed = self.make_seed()
+                    print("Your wallet generation seed is:\n\"%s\"" % seed)
+                ks = keystore.from_seed(seed, passphrase, False)
+
+            storage.put('keystore', ks.dump())
+            wallet = Standard_Wallet(storage)
+
+        wallet.update_password(None, password, True)
+
+    # END commands from the argparse interface.
+
+    # BEGIN commands which only exist here.
+
+    def select_wallet(self, name):
+        if name is None:
+            self.wallet = None
+        else:
+            self.wallet = self.daemon.wallets[self._wallet_path(name)]
+        self.network.trigger_callback("wallet_updated", self.wallet)
+
+    def list_wallets(self):
+        """List available wallets"""
+        return sorted([name for name in os.listdir(self._wallet_path())
+                       if not name.endswith(storage.TMP_SUFFIX)])
+
+    def delete_wallet(self, name=None):
+        """Delete a wallet"""
+        os.remove(self._wallet_path(name))
+
+    def unit_test(self):
+        """Run all unit tests. Expect failures with functionality not present on Android,
+        such as Trezor.
+        """
+        suite = unittest.defaultTestLoader.loadTestsFromNames(
+            tests.__name__ + "." + info.name
+            for info in pkgutil.iter_modules(tests.__path__)
+            if info.name.startswith("test_"))
+        unittest.TextTestRunner(verbosity=2).run(suite)
+
+    # END commands which only exist here.
+
+    def _assert_daemon_running(self):
+        if not self.daemon_running:
+            raise Exception("Daemon not running")  # Same wording as in electron-cash script.
+
+    # Log callbacks on stderr so they'll appear in the console activity.
+    def _on_callback(self, *args):
+        util.print_stderr("[Callback] " + ", ".join(repr(x) for x in args))
+
+    def _wallet_path(self, name=""):
+        if name is None:
+            if not self.wallet:
+                raise ValueError("No wallet selected")
+            return self.wallet.storage.path
+        else:
+            wallets_dir = join(util.user_dir(), "wallets")
+            util.make_dir(wallets_dir)
+            return util.standardize_path(join(wallets_dir, name))
+
+
+all_commands = commands.known_commands.copy()
+for name, func in vars(AndroidCommands).items():
+    if not name.startswith("_"):
+        all_commands[name] = commands.Command(func, "")
+
+
+SP_SET_METHODS = {
+    bool: "putBoolean",
+    float: "putFloat",
+    int: "putLong",
+    str: "putString",
+}
+
+# We store the config in the SharedPreferences because it's very easy to base an Android
+# settings UI on that. The reverse approach would be harder (using PreferenceDataStore to make
+# the settings UI access an Electron Cash config file).
+class AndroidConfig(simple_config.SimpleConfig):
+    def __init__(self, app):
+        self.sp = PreferenceManager.getDefaultSharedPreferences(app)
+        super().__init__()
+
+    def get(self, key, default=None):
+        if self.sp.contains(key):
+            value = self.sp.getAll().get(key)
+            if value == "<json>":
+                json_value = self.sp.getString(key + ".json", None)
+                if json_value is not None:
+                    value = json.loads(json_value)
+            return value
+        else:
+            return default
+
+    def set_key(self, key, value, save=None):
+        spe = self.sp.edit()
+        if value is None:
+            spe.remove(key)
+            spe.remove(key + ".json")
+        else:
+            set_method = SP_SET_METHODS.get(type(value))
+            if set_method:
+                getattr(spe, set_method)(key, value)
+            else:
+                spe.putString(key, "<json>")
+                spe.putString(key + ".json", json.dumps(value))
+        spe.apply()
