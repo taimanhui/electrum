913a9c1cfdb425751ccb13e260947ca9b59762ee
xiaoliang <137569752@qq.com>
2021-01-13 16:26:57 +0800

    Delete Model Comments

--- ios/CustomCode/Modules/Hardware/Model/OKDeviceInfoModel.h
+++ ios/CustomCode/Modules/Hardware/Model/OKDeviceInfoModel.h
@@ -6,48 +6,6 @@
 //  Copyright © 2021 Onekey. All rights reserved.
 //
 
-/**
- {
-     "auto_lock_delay_ms" = 1800000;
-     "backup_only" = 0;
-     "ble_enable" = 1;
-     "ble_name" = K8543;
-     "ble_ver" = "1.1.8";
-     "bootloader_hash" = b532d75a3c385d73ba58b82991e836d126eab45bb387100bc6b4f74805b09fb0;
-     capabilities =     (
-         Bitcoin,
-         "Bitcoin_like",
-         Crypto,
-         Ethereum,
-         Lisk,
-         NEM,
-         Stellar,
-         U2F
-     );
-     "device_id" = 398F8DF436D2C78513E9580C;
-     initialized = 1;
-     label = "BIXIN KEY";
-     language = chinese;
-     "major_version" = 1;
-     "minor_version" = 9;
-     model = 1;
-     "needs_backup" = 0;
-     "no_backup" = 0;
-     "passphrase_protection" = 1;
-     "patch_version" = 8;
-     "pin_protection" = 1;
-     revision = 450a9d866b388d9fbb3d0e65d8b1211e59c31273;
-     "se_enable" = 0;
-     "se_ver" = "1.0.0.4";
-     "unfinished_backup" = 0;
-     unlocked = 1;
-     vendor = "trezor.io";
-     "wipe_code_protection" = 0;
- }
- 
- */
-
-
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
