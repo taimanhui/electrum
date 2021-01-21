90d8513e64257744ec5d2fed3ce190193ff999fa
liyanhrxy <sweepmonkli@gmail.com>
2021-01-21 17:37:30 +0800

    add eth_hash to ios/Support

--- ios/setup.py
+++ ios/setup.py
@@ -54,8 +54,8 @@ setup(
         'libusb1==1.7.1', 'libusb==1.0.22b2', 'importlib-metadata==1.6.1', 'multidict==4.7.4', 'protobuf==3.12.2', 'pyaes==1.6.1', 'QDarkStyle==2.8.1',
         'qrcode==6.1', 'QtPy==1.9.0', 'six==1.15.0', 'typing-extensions==3.7.4.2', 'yarl==1.4.2', 'zipp==3.1.0', 'colorama==0.4.3',
         'coincurve==13.0.0', 'pycparser==2.20', 'asn1crypto==0.24.0', 'web3==5.13.0', 'eth-account==0.5.4', 'eth-keys==0.3.3', 'eth-utils==1.9.5', 'lru-dict==1.1.6',
-        'eth-abi==2.1.1', 'hexbytes==0.2.1', 'cytoolz==0.11.0', 'eth-hash==0.2.0', 'eth-rlp==0.2.1', 'attrdict==2.0.1', 'eth-typing==2.2.2',
-        'parsimonious==0.8.1',  'rlp==2.0.0', 'rubicon-objc==0.4.0',  # 'cffi==1.13.2',
+        'eth-abi==2.1.1', 'hexbytes==0.2.1', 'cytoolz==0.11.0', 'eth-rlp==0.2.1', 'attrdict==2.0.1', 'eth-typing==2.2.2',
+        'parsimonious==0.8.1',  'rlp==2.0.0', 'rubicon-objc==0.4.0',  # 'cffi==1.13.2', 'eth-hash==0.2.0',
     ],
     options={
         'app': {
