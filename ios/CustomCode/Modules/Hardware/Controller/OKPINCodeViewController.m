da710f8c84dbfc765857dde58ce77411fc15e048
xiaoliang <137569752@qq.com>
2021-02-05 15:20:19 +0800

    iOS feature：Remove the digit limit for PIN codes

--- ios/CustomCode/Modules/Hardware/Controller/OKPINCodeViewController.m
+++ ios/CustomCode/Modules/Hardware/Controller/OKPINCodeViewController.m
@@ -26,11 +26,14 @@ static NSString *PIN_ON_DEVICE_CODE = @"000000";
 
 @property (weak, nonatomic) IBOutlet UIButton *confirmBtn;
 
-@property (nonatomic, strong)OK_PassWordView *pwdView;
-
 @property (nonatomic, copy)PINCodeComplete complete;
 
 @property (assign, nonatomic) BOOL inputPINOnDevice;
+
+@property (weak, nonatomic) IBOutlet UITextField *pwdAppInputTextField;
+@property (weak, nonatomic) IBOutlet UIButton *eyeBtn;
+- (IBAction)eyeBtnClick:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIView *longPwdBgView;
 
 @end
 
@@ -52,6 +55,7 @@ static NSString *PIN_ON_DEVICE_CODE = @"000000";
     self.title = MyLocalizedString(@"Check the PIN code", nil);
 
     self.iconImageView.image = [UIImage imageNamed:@"Rectangle"];
+    [self.dotBgView setLayerBoarderColor:HexColor(0xDBDEE7) width:1 radius:20];
     if (self.titleLabelText) {
         self.titleLabel.text = self.titleLabelText;
     } else if (self.inputPINOnDevice) {
@@ -64,30 +68,18 @@ static NSString *PIN_ON_DEVICE_CODE = @"000000";
         self.deviceInputTip.text = MyLocalizedString(@"hardwareWallet.pin.verifyMethodOnDeviceTip", nil);
         self.descLabel.hidden = YES;
         self.iconImageView.hidden = YES;
-        self.pwdView.hidden = YES;
+        self.dotBgView.hidden = YES;
         self.confirmBtn.hidden = YES;
         self.dotBgView.hidden = YES;
         self.keyBoardBgView.hidden = YES;
     } else {
         self.descLabel.text = MyLocalizedString(@"The number keys on the phone change randomly every time. The PIN number is not retrievable. You must keep it in mind", nil);
         [self.confirmBtn setTitle:MyLocalizedString(@"confirm", nil) forState:UIControlStateNormal];
-        self.pwdView.userInteractionEnabled = NO;
-
-        OK_PassWordView *pwdView = [[OK_PassWordView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH - 20 * 2, (SCREEN_WIDTH - 20 * 2 - 11 * 5)/6)];
-        pwdView.showType = passShow3;
-        pwdView.num = 6;
-        pwdView.tintColor = [UIColor whiteColor];
-        pwdView.textBlock = ^(NSString *str) {
-            //NSLog(@"str == %@",str);
-        };
-        self.pwdView = pwdView;
-        [pwdView show];
-        [self.dotBgView addSubview:pwdView];
-
+        self.dotBgView.userInteractionEnabled = NO;
+        self.dotBgView.hidden = NO;
         self.deviceInputImage.hidden = YES;
         self.deviceInputTip.hidden = YES;
     }
-
 
     if (self.inputPINOnDevice && self.complete) {
         self.complete(PIN_ON_DEVICE_CODE);
@@ -96,7 +88,7 @@ static NSString *PIN_ON_DEVICE_CODE = @"000000";
 
 
 - (IBAction)keyBtnClick:(UIButton *)sender {
-    NSMutableString *oldStr = [NSMutableString stringWithString:self.pwdView.textF.text];
+    NSMutableString *oldStr = [NSMutableString stringWithString:self.pwdAppInputTextField.text];
     NSString *newStr = @"";
     if (sender.tag == 1012) {
         //删除
@@ -106,7 +98,7 @@ static NSString *PIN_ON_DEVICE_CODE = @"000000";
             newStr = [oldStr substringToIndex:0];
         }
     }else if (sender.tag == 1011){
-        if (self.complete && oldStr.length == 6) {
+        if (self.complete && oldStr.length >= 1 && oldStr.length <= 9) {
             self.complete(oldStr);
         }else{
             [kTools tipMessage:MyLocalizedString(@"Enter your 6-digit password according to the PIN code location comparison table displayed on the device", nil)];
@@ -115,13 +107,16 @@ static NSString *PIN_ON_DEVICE_CODE = @"000000";
         [oldStr appendString:[NSString stringWithFormat:@"%zd",sender.tag - 1000]];
         newStr = oldStr;
     }
-    if (newStr.length > 6) {
+    if (newStr.length > 9) {
         return;
     }
-    [self.pwdView textFieldChanged:newStr];
+    self.pwdAppInputTextField.text = newStr;
 }
 
 - (BOOL)inputPINOnDevice {
     return kUserSettingManager.pinInputMethod == OKDevicePINInputMethodOnDevice;
+}
+- (IBAction)eyeBtnClick:(UIButton *)sender {
+    NSLog(@"eyeBtnClick");
 }
 @end
