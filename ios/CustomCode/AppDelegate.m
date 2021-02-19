4b3d685f2fe96f05346e3b282da3336eca2c3b1a
LiuZJ <hust_lzj@icloud.com>
2021-02-19 19:46:34 +0800

    iOS feature: support jpush.

--- ios/CustomCode/AppDelegate.m
+++ ios/CustomCode/AppDelegate.m
@@ -8,8 +8,11 @@
 #import "MainViewController.h"
 #import "AppDelegate.h"
 #import "OKFirstUseViewController.h"
+#import "JPUSHService.h"
+#import <UserNotifications/UserNotifications.h>
+#import <AdSupport/AdSupport.h>
 
-@interface AppDelegate ()
+@interface AppDelegate () <JPUSHRegisterDelegate>
 @property (nonatomic,strong)MainViewController *mainVC;
 @end
 
@@ -20,13 +23,18 @@
 #ifdef DEBUG
         [OKPyCommandsManager setNetwork];
 #endif
-    
+
     [self setupLanague];
-    
+    [self setupJPUSH:launchOptions];
+
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.rootViewController = self.mainVC;
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [JPUSHService registerDeviceToken:deviceToken];
 }
 
 - (void)setupLanague
@@ -50,5 +58,61 @@
     self.mainVC.isTabSettingVC = isSettingVC;
     self.window.rootViewController = self.mainVC;
 }
+
+- (void)setupJPUSH:(NSDictionary *)launchOptions {
+
+    JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];
+    if (@available(iOS 12.0, *)) {
+        entity.types = JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound|JPAuthorizationOptionProvidesAppNotificationSettings;
+    } else {
+        entity.types = JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound;
+    }
+    [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
+
+    NSString *advertisingId = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
+    [JPUSHService setupWithOption:launchOptions
+                             appKey:@"8deab53d077ef22770b7521a"
+                            channel:@"NA"
+                   apsForProduction:NO
+              advertisingIdentifier:advertisingId];
+}
+#pragma mark- JPUSHRegisterDelegate
+
+// iOS 12 Support
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center openSettingsForNotification:(UNNotification *)notification{
+  if (notification && [notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+    //从通知界面直接进入应用
+  }else{
+    //从通知设置界面进入应用
+  }
+}
+
+// iOS 10 Support
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler {
+  // Required
+  NSDictionary * userInfo = notification.request.content.userInfo;
+  if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+    [JPUSHService handleRemoteNotification:userInfo];
+  }
+  completionHandler(UNNotificationPresentationOptionAlert); // 需要执行这个方法，选择是否提醒用户，有 Badge、Sound、Alert 三种类型可以选择设置
+}
+
+// iOS 10 Support
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
+  // Required
+  NSDictionary * userInfo = response.notification.request.content.userInfo;
+  if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+    [JPUSHService handleRemoteNotification:userInfo];
+  }
+  completionHandler();  // 系统要求执行这个方法
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+
+  // Required, iOS 7 Support
+  [JPUSHService handleRemoteNotification:userInfo];
+  completionHandler(UIBackgroundFetchResultNewData);
+}
+
 
 @end
