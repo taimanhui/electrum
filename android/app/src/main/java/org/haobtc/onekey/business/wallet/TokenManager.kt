fe737761c149625a04dd4d8780fecb5df523481a
QuincySx <QuincySx@sina.com>
2021-03-10 13:33:06 +0800

    Android:Wallet fixed add custom ERC20 not showing

--- android/app/src/main/java/org/haobtc/onekey/business/wallet/TokenManager.kt
+++ android/app/src/main/java/org/haobtc/onekey/business/wallet/TokenManager.kt
@@ -1,5 +1,6 @@
 package org.haobtc.onekey.business.wallet
 
+import android.util.Log
 import androidx.annotation.WorkerThread
 import com.alibaba.fastjson.JSON
 import com.google.common.base.Strings
@@ -11,14 +12,31 @@ import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
 import java.io.IOException
+import java.lang.ref.ReferenceQueue
+import java.lang.ref.SoftReference
+import java.util.*
 import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.collections.HashMap
 import kotlin.concurrent.read
 import kotlin.concurrent.write
 
 /** @Description: java类作用描述 @Author: peter Qin
  */
 class TokenManager {
+  private val mReferenceQueue = ReferenceQueue<ERCToken>()
 
+  private val mTokenSoftReferenceMap = HashMap<String, SoftReference<ERCToken>>(1100)
+
+
+  private fun cleanCache() = mReadWriteLock.write {
+    var se = mReferenceQueue.poll()
+    while (se != null) {
+      se.get()?.address?.toLowerCase(Locale.ROOT)?.let {
+        mTokenSoftReferenceMap.remove(it)
+      }
+      se = mReferenceQueue.poll()
+    }
+  }
 
   /**
    * 如果没网，就用本地文件存储的 TokenList 展示 如果能从服务器拿到数据，判断是否需要更新本地文件
@@ -57,9 +75,16 @@ class TokenManager {
 
   @WorkerThread
   fun initFile() {
+    Log.e("TokenManager", "init file")
     mReadWriteLock.write {
       val response = PyEnv.getAllTokenInfo()
       uploadLocalTokenList(response.result)
+
+      JSON.parseArray(response.result, ERCToken::class.java).forEach {
+
+        mTokenSoftReferenceMap[it.address.toLowerCase(Locale.ROOT)] = SoftReference(it, mReferenceQueue)
+      }
+      Log.e("TokenManager", "init file done")
     }
   }
 
@@ -82,6 +107,7 @@ class TokenManager {
       }
       return null
     }
+
   val tokenList: List<ERCToken>
     get() = mReadWriteLock.read {
       JSON.parseArray(localTokenList, ERCToken::class.java)
@@ -94,12 +120,26 @@ class TokenManager {
    * @return
    */
   fun getTokenByAddress(address: String?): ERCToken? = mReadWriteLock.read {
-    val tokenList = JSON.parseArray(localTokenList, ERCToken::class.java)
-    for (token in tokenList) {
-      if (!Strings.isNullOrEmpty(token.address) && !Strings.isNullOrEmpty(address)) {
-        if (token.address.equals(address, ignoreCase = true)) {
-          return token
-        }
+    val softReference = mTokenSoftReferenceMap[address?.toLowerCase(Locale.ROOT)]
+    var customERCToken: ERCToken? = null
+    if (softReference?.get() == null) {
+      customERCToken = getCustomTokenByAddress(address)
+      if (customERCToken == null) {
+        cleanCache()
+        initFile()
+      }
+    }
+    return mTokenSoftReferenceMap[address?.toLowerCase(Locale.ROOT)]?.get() ?: customERCToken
+  }
+
+  private fun getCustomTokenList(): MutableList<ERCToken> {
+    return PyEnv.getTokenList().result ?: arrayListOf()
+  }
+
+  private fun getCustomTokenByAddress(address: String?): ERCToken? = mReadWriteLock.write {
+    getCustomTokenList().forEach {
+      if (it.address.equals(address, true)) {
+        return it
       }
     }
     return null
