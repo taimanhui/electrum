7771076854c5e8c8819c5949c3a27232587e6255
huazhouwang <huazhou19@gmail.com>
2021-03-14 20:34:30 +0800

    timing logger and ticker signal

---
+++ electrum_gui/common/basic/ticker/__init__.py
@@ -0,0 +1,17 @@
+import logging
+
+from electrum_gui.common.basic.ticker import signals, ticker
+
+logger = logging.getLogger("app.ticker")
+
+_ticker = None
+
+
+def start_default_ticker(seconds: int):
+    global _ticker
+    if _ticker is not None:
+        logger.warning("start ticker already")
+        return
+
+    _ticker = ticker.Ticker(seconds, signals.ticker_signal)
+    _ticker.start()
--- electrum_gui/common/the_begging.py
+++ electrum_gui/common/the_begging.py
@@ -11,3 +11,10 @@ def initialize():
     from electrum_gui.common.basic.orm.migrate import manager
 
     manager.migrating(db)
+
+    # from electrum_gui.common.basic import ticker
+
+    # connect ticker signal here before start
+    # example ticker.signals.ticker_signal.connect(my_ticker_callback)
+    # start ticker
+    # ticker.start_default_ticker(seconds=60)  # every 1 min
