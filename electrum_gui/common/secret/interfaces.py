f078679b4a0c805bdc106d2ebc43e3fc003dc1db
huazhouwang <huazhou19@gmail.com>
2021-04-08 20:55:16 +0800

    stub secret interfaces

---
+++ electrum_gui/common/secret/interfaces.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+
+class VerifierInterface(ABC):
+    @abstractmethod
+    def pubkey(self, compressed=True) -> bytes:
+        pass
+
+
+class SignerInterface(VerifierInterface, ABC):
+    @abstractmethod
+    def sign(self, digest: bytes) -> Tuple[bytes, int]:
+        pass
