8f5370cde94fce918fbf6a966da1c55b84ad8aa4
wls <wanglishuai_210@sina.com>
2021-04-25 01:56:25 -0700

    Recovered wallet name delete -1

--- electrum_gui/android/console.py
+++ electrum_gui/android/console.py
@@ -3513,9 +3513,9 @@ class AndroidCommands(commands.Commands):
         if account_id == 0:
             purpose = int(helpers.get_path_info(bip39_derivation, PURPOSE_POS))
             if purpose == 49:
-                name = "BTC-1"
+                name = "BTC"
             elif coin in self.coins:
-                name = "%s-1" % coin.upper()
+                name = "%s" % coin.upper()
             else:
                 name = "btc-derived-%s" % purpose
         temp_path = helpers.get_temp_file()
