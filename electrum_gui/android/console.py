03a9b99215b81884aa6d01f1258180f7e0a99694
wls <wanglishuai_210@sina.com>
2021-05-09 23:53:42 -0700

    No need to update the balance of your wallet when the network is updated

--- electrum_gui/android/console.py
+++ electrum_gui/android/console.py
@@ -397,8 +397,10 @@ class AndroidCommands(commands.Commands):
     def on_network_event(self, event, *args):
         if event == "set_server_status" and self.callbackIntent is not None:
             self.callbackIntent.onCallback("set_server_status=%s" % args[0])
+        elif event in ("network_updated"):
+            # TODO: Update the list of available servers
+            pass
         elif event in (
-            "network_updated",
             "wallet_updated",
             "blockchain_updated",
             "status",
