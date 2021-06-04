89b936ad0423190387dbb2e1f82a7440e89b6cf7
wls <lishuaiw21@gmail.com>
2021-06-04 04:20:55 -0700

    Solve the problem of failing to get the default fee information under etherscan

--- electrum_gui/android/console.py
+++ electrum_gui/android/console.py
@@ -915,6 +915,7 @@ class AndroidCommands(commands.Commands):
         }
         return ret_data
 
+    @exceptions.catch_exception
     def get_default_fee_info(self, feerate=None, coin="btc", eth_tx_info=None):
         """
         Get default fee info for btc
